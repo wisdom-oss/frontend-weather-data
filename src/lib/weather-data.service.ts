7fa58103222fcd03e8e3d68284407e5300baee94
zoax3155 <stueven.jan@web.de>
2023-11-10 13:18:26 +0100

    ground architecture

--- src/lib/weather-data.service.ts
+++ src/lib/weather-data.service.ts
@@ -1,9 +1,21 @@
-import {Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpContext } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { USE_API_URL, USE_LOADER, USE_ERROR_HANDLER } from "common";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherDataService {
 
-  constructor() { }
+  /**
+   * http context for further information to the request.
+   */
+  ctx: HttpContext = new HttpContext()
+    .set(USE_API_URL, true)
+    .set(USE_LOADER, false)
+    .set(USE_ERROR_HANDLER, 1);
+
+  constructor(private http: HttpClient, private router: Router) { }
 }
