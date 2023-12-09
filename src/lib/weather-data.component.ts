280e7a1a67b6c6e9e728b99a44bad6e2b16ff855
Jan <stueven.jan@web.de>
2023-12-09 23:02:00 +0100

    check

--- src/lib/weather-data.component.ts
+++ src/lib/weather-data.component.ts
@@ -3,6 +3,7 @@ import { Marker } from "common";
 import { dwdStationIcon } from "./map-icons";
 import { WeatherDataService } from "./weather-data.service";
 import { Stations } from "./dwd-interfaces";
+import { Chart } from "chart.js/auto";
 
 @Component({
   selector: 'lib-weather-data',
@@ -21,8 +22,6 @@ export class WeatherDataComponent implements OnInit {
 
   weatherData: any;
 
-  possibleResolutions!: any;
-
   constructor(public weatherService: WeatherDataService) { }
 
   ngOnInit(): void {
@@ -40,7 +39,7 @@ export class WeatherDataComponent implements OnInit {
       this.stationMarkers = Array.from(this.stations.map(station => {
         return {
           coordinates: station.location.coordinates,
-          tooltip: station.name,
+          tooltip: station.id,
           icon: dwdStationIcon
         }
       }));
@@ -51,10 +50,29 @@ export class WeatherDataComponent implements OnInit {
    * test function with valid values to check api
    */
   testWeatherData(): void {
-    this.processApiParams("00044", "air_temperature", "10_minutes","1701965435", "1701967435")
+    this.buildUrl("00044", "air_temperature", "10_minutes","1701965435", "1701967435")
   }
 
   /**
+   * function to request weather data from station
+   * @param stationId id of station
+   * @param dataPoint kind of data
+   * @param timeResolution timely resolution
+   * @param from first timestamp
+   * @param until last timestamp
+   */
+  requestDataforStation(stationId: string, dataPoint: string, timeResolution: string, from?: string, until?: string): void {
+    let url = this.buildUrl(stationId, dataPoint, timeResolution, from, until);
+
+    if(url) {
+      this.getWeatherDataByStation(url);
+    } else {
+      console.log("Failed");
+    }   
+  }
+
+  /**
+   * help function, not to be called directly
    * standardized processing function to build the url to retrieve stationary data
    * @param stationId to find a unique station
    * @param dataPoint kind of data to retrieve (precipitation etc.)
@@ -63,11 +81,11 @@ export class WeatherDataComponent implements OnInit {
    * @param until timestamp until data is requested
    * @returns if no stationID is used to safe resources
    */
-  processApiParams(stationId: string, dataPoint: string, timeResolution: string, from?: string, until?: string): void {
+  buildUrl(stationId: string, dataPoint: string, timeResolution: string, from?: string, until?: string): string | undefined{
     if (!stationId) {
       //TODO raise Error here
       console.log("No valid ID");
-      return;
+      return undefined;
     }
 
     let endpoint: string = `/${stationId}/${dataPoint}/${timeResolution}`
@@ -79,10 +97,11 @@ export class WeatherDataComponent implements OnInit {
       console.log("No period specified, request can take longer!");
     }
 
-    this.getWeatherDataByStation(endpoint);
+    return endpoint;
   }
 
   /**
+   * help function not to be called directly
    * request the weather service for a station, time resolution and data point
    * @param url the finished url to request from
    */
@@ -102,6 +121,12 @@ export class WeatherDataComponent implements OnInit {
     })
   }
 
+  /**
+   * help function to find the id from a name and state
+   * @param name name of the location
+   * @param state of the location
+   * @returns dwd id from station
+   */
   searchIdFromNameAndState(name: string, state: string): string | undefined {
     let station: any;
     if (this.stations) {
@@ -112,6 +137,36 @@ export class WeatherDataComponent implements OnInit {
       return undefined;
     }
     return station.id;
+
+  }
+
+  createBasicBarChart(): void {
+
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+  
+    new Chart(
+      document.getElementById('weather') as HTMLCanvasElement,
+      {
+        type: 'bar',
+        data: {
+          labels: data.map(column => column.year),
+          datasets: [
+            {
+              label: 'Acquisitions by year',
+              data: data.map(column => column.count)
+            }
+          ]
+        }
+      }
+    );
 
   }
 
